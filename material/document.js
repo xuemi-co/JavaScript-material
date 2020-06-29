5f3971fc8f8625960df2d2a627a21f22a733adf9
YouMinTW <m791213p@gmail.com>
2020-06-30 02:03:50 +0800

    update document description

--- material/document.js
+++ material/document.js
@@ -1,8 +1,8 @@
 // console.log(document)
 // console.log(document.head)
 
-console.log(document.body)
-// 如果<script>是放在head的話，將無法執行這段程式碼
+console.log(document.body)// 如果<script>是放在head的話，body顯示null
+// 如果<script>是放在head的話，將無法執行以下這段程式碼
 // document.body.innerHTML='<button>快來點我</button><h1>原本的內容被覆寫掉了</h1>'
 // 看不懂這段程式碼沒關係、我們後續會教到
 // 讓我們先回投影片，來看看為什麼放body才有作用
